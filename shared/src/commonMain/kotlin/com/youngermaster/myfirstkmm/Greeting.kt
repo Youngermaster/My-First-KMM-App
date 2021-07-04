6ca437970f266a23e677c0b5512946f9c114ac46
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-04 18:13:24 -0500

    Greetins updated

--- shared/src/commonMain/kotlin/com/youngermaster/myfirstkmm/Greeting.kt
+++ shared/src/commonMain/kotlin/com/youngermaster/myfirstkmm/Greeting.kt
@@ -2,6 +2,6 @@ package com.youngermaster.myfirstkmm
 
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+        return "Guess what it is! > ${Platform().platform.reversed()}!"
     }
 }
